9ed097e93731eeca2b0f33378e5df381b3d982f9
Steven R <stevenr@MacBook-Air-Steven-4.local>
2025-10-18 12:57:01 +0100

    Terminada la primera mitad de la practica 3

---
+++ 3-Web_components/ejercicio1/componentes.js
@@ -0,0 +1,53 @@
+// Extraer referencia de la plantilla para poder crear el elemento
+const plantilla = document.getElementById('botones-webComponent');
+
+// Definir la clase del web component
+class WebComponent extends HTMLElement {
+    // Constructor del web component
+    constructor() {
+        super();
+        console.log('Creado'); // Mensaje en consola al crear el componente
+        // Adjuntar un shadow DOM
+        this.attachShadow({ mode: 'open' });
+        // Clonar el contenido de la plantilla y adjuntarlo al shadow DOM
+        const clone = plantilla.content.cloneNode(true);
+        this.shadowRoot.appendChild(clone);
+        // Coger una referencia al botón con id anyadir-webComponent que están en el elemento creado.
+        this.button = this.shadowRoot.querySelector('#anyadir-webComponent');
+        // Coger una referencia al botón con id cambiar-webComponent que están en el elemento creado.
+        this.changeButton = this.shadowRoot.querySelector('#cambiar-webComponent');
+
+    }
+
+    // Se requiere declarar los atributos observados para que el navegador llame a attributeChangedCallback
+    static get observedAttributes() { 
+        return ['state']; 
+    }
+
+    connectedCallback() {
+        console.log("Conectado");
+        // Añadir un nuevo web component al hacer click en el botón Añadir
+        this.button.addEventListener('click', () => {
+            const nuevoComponente = document.createElement('web-component');
+            document.getElementById('Area-webComponent').appendChild(nuevoComponente);
+        });
+
+        // Cambiar un atributo simple al hacer click en el botón Cambiar
+        this.changeButton.addEventListener('click', () => {
+            const estadoActual = this.getAttribute('state') === 'on' ? 'off' : 'on'; // Alternar entre 'on' y 'off'
+            this.setAttribute('state', estadoActual); // Cambiar el atributo 'state' al siguiente estado
+        });
+    }
+    disconnectedCallback() {
+        console.log("Desconectado");
+    }
+    adoptedCallback() {
+        console.log("Creado");
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(`Atributo cambiado ${name} -> ${newValue}`);
+    }
+}
+
+// Registrar el web component con el nombre 'web-component'
+customElements.define('web-component', WebComponent);
--- README.md
+++ README.md
@@ -6,5 +6,6 @@ En este repositorio se almacenarán todas las prácticas de la asignatura durant
 
 ##### Listado de prácticas:
 
-1. Introducción a Sass
-2. Proyecto Gulp
+1. **Introducción a Sass**: Ejercicios básicos de introducción al lenguaje Sass, y al uso de los organizadores Flex y Grid.
+2. **Proyecto Gulp**: Práctica basada en el uso de varias herramientas de automatización y *bundling*, con el objetivo de conocer su funcionamiento y las ventajas que proporcionan.
+3. **Web components**: Basándose en la replicación de un sistema de puntuación de eventos culturales, se busca crear aplicaciones web sencillas donde se demuestre la eficacia en el uso de web components. 
